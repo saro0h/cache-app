c42b4be7ba21ae5e7da115ecd3d2749c38ea361c
Saro0h <mkhalil.sarah@gmail.com>
2014-10-19 21:51:49 +0200

    Added maxage

--- src/Saro0h/AppBundle/Controller/DefaultController.php
+++ src/Saro0h/AppBundle/Controller/DefaultController.php
@@ -5,12 +5,14 @@ namespace Saro0h\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      * @Template()
+     * @cache(maxage=120)
      */
     public function indexAction()
     {
